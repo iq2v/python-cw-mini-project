b83fcb0723db9c5b1b9d66eaef24ba067d5f0037
iq2v <142683864+iq2v@users.noreply.github.com>
2023-09-12 18:48:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+#part1
 def padel_court_cost(court_type) :
      if court_type == "indoor" :
           return 30 
-     elif court_type == "outdoor"
-     return 20 
+     elif court_type == 'outdoor': 
+       return 20 
+#part2
 
 def rackets_cost(racket_brand) : 
      if racket_brand == " bullpadel" : 
@@ -12,19 +14,25 @@ def rackets_cost(racket_brand) :
      elif racket_brand == "suix" :
           return 200
      
+#part3
+
      def padel_balls_cost(ball_boxes) :
-          if ball_boxes == "one" : 
+          if ball_boxes == "one": 
                return 2
-          elif ball_boxes == "two" :
-          elif ball_boxes == "three" 
+          elif ball_boxes == "two":
+               return 3.5
+          elif ball_boxes == "three" :
                return 5 
-          
-    def padel_game_cost():
-     court_type= input("your preferd courttype:")
-     racket_brand= input("your preferd racket brand:")
-     ball_boxes= input(int("number of balls boxes you need:"))
 
-     total_cost= padel_court_cost(court_type) + rackets_cost(racket_brand)+ padel_balls_cost(ball_boxes)
-     return total_cost
+#part4 
 
-print(padel_game_cost{})
+
+
+def padel_game_cost():
+     court_type = input("would you like an idoor court or outdoor?:")
+     racket_brand = input("racket brand options (bullpadel/nox/suix) please choose:")
+     ball_boxes = input(int("how many ball boxes do you need?:(max :3)"))
+     cost = padel_court_cost(court_type) + rackets_cost(racket_brand)+ padel_balls_cost(ball_boxes)
+     return cost
+
+print(padel_game_cost())
