1aee799df256c9b6ef6b7040a159570941b3a70d
iq2v <142683864+iq2v@users.noreply.github.com>
2023-09-12 20:09:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,32 @@
-#part1
-def padel_court_cost(court_type) :
-     if court_type == "indoor" :
-          return 30 
-     elif court_type == 'outdoor': 
-       return 20 
-#part2
+def padel_court_cost(court_type):
+    if court_type =="indoor":
+        return(30)
+    elif court_type == "outdoor":
+        return(20)
+    else: False
 
-def rackets_cost(racket_brand) : 
-     if racket_brand == " bullpadel" : 
-          return 100
-     elif racket_brand == " nox" : 
-          return 140 
-     elif racket_brand == "suix" :
-          return 200
-     
-#part3
+def  rackets_cost(rackets_brand):
+    if rackets_brand== ("Bullpadel"):
+            return(100)
+    if rackets_brand== ("Nox"):
+            return(140)
+    if rackets_brand== ("siux"):
+            return(200)
 
-     def padel_balls_cost(ball_boxes) :
-          if ball_boxes == "one": 
-               return 2
-          elif ball_boxes == "two":
-               return 3.5
-          elif ball_boxes == "three" :
-               return 5 
-
-#part4 
-
-
+def padel_balls_cost(ball_boxes):
+    if ball_boxes == 1 :
+        return 2
+    if ball_boxes == 2 :
+        return 3.5
+    if ball_boxes == 3 :
+        return 5
 
 def padel_game_cost():
-     court_type = input("would you like an idoor court or outdoor?:")
-     racket_brand = input("racket brand options (bullpadel/nox/suix) please choose:")
-     ball_boxes = input(int("how many ball boxes do you need?:(max :3)"))
-     cost = padel_court_cost(court_type) + rackets_cost(racket_brand)+ padel_balls_cost(ball_boxes)
-     return cost
 
-print(padel_game_cost())
+    court_type = input("court type indoor: / outdoor")
+    rackets_brand = input("racket_brand : nox / bullpadel / siux")
+    ball_boxes = int(input("number of ball boxes: 1 / 2 / 3"))
+    print = padel_court_cost(court_type) + rackets_cost(rackets_brand) + padel_balls_cost(ball_boxes)
+    return print
+
+print (padel_game_cost())
